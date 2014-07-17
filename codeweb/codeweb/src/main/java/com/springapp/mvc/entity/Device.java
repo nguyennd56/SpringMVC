4386104474aec3545545012f5cc38c2c041673f5
Ngô Đình Nguyên <nguyennd56coltech@gmail.com>
2014-07-17 17:24:34 +0700

    simple web with 3 layers form
    function: add devices + show list added devices + delete added device in
    list

---
+++ codeweb/codeweb/src/main/java/com/springapp/mvc/entity/Device.java
@@ -0,0 +1,30 @@
+package com.springapp.mvc.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "deviceManager")
+public class Device
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column (name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ codeweb/codeweb/src/main/java/com/springapp/mvc/service/DeviceService.java
@@ -0,0 +1,63 @@
+package com.springapp.mvc.service;
+
+import com.springapp.mvc.dao.DeviceRepository;
+import com.springapp.mvc.entity.Device;
+import com.springapp.mvc.model.DeviceModel;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nguyennd on 7/17/2014.
+ */
+@Service
+public class DeviceService {
+
+    @Autowired
+    DeviceRepository deviceRepository;
+
+    public List<DeviceModel> listDevices()
+    {
+        List<Device> deviceList = deviceRepository.findAll();
+
+        List<DeviceModel> deviceModelList = new ArrayList<DeviceModel>();
+
+        for(Device device : deviceList)
+        {
+            DeviceModel deviceModel = new DeviceModel();
+            deviceModel.setId(device.getId());
+            deviceModel.setName(device.getName());
+
+            deviceModelList.add(deviceModel);
+        }
+
+        return  deviceModelList;
+    }
+    public String jsonList()throws JSONException
+    {
+        JSONArray userArray = new JSONArray();
+
+
+        for (Device device : deviceRepository.findAll())
+        {
+            JSONObject deviceJSON = new JSONObject();
+            deviceJSON.put("id",device.getId());
+            deviceJSON.put("name", device.getName());
+            userArray.put(deviceJSON);
+        }
+        return userArray.toString();
+    }
+    public void save(Device device){
+        deviceRepository.save(device);
+    }
+
+    public void delete(long id){
+        deviceRepository.delete(deviceRepository.findOne(id));
+    }
+
+}
